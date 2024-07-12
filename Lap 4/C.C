bc335040018bd6ec125875dfb3ac0f148b8859ac
sansukai <lobaoduy2017@gmail.com>
2024-07-13 00:15:02 +0700

    update 1 file and create 1 file

--- Lap 4/C.C
+++ Lap 4/C.C
@@ -1,20 +1,50 @@
 #include<stdio.h>
 
-int top = - 1 ;
+int top = -1; 
 
 
-void push(int num , int*arr){
+void push(int num,int *arr){
     top ++;
     *(arr + top) = num;
 }
 
+void pop(){
+    top --;
+}
+
+int peek(int *arr){
+return *(arr+top);
+}
+
 int main(){
-    int n ; scanf("%d",&n);
+    int n ; scanf("%d",&n); int num;
     int arr[n];
     int hoanvi[n];
 
     for(int i = 0 ; i < n ; i ++){
-        num;
+        scanf("%d",&num);
+        push(num,arr);
     }
 
+    for(int i = 0 ; i < n ; i++){
+        int sum = 0;
+        for(int j = 0 ; j <= arr[i] ; j ++){
+            sum += j ;
+        }
+        hoanvi[i] = sum;
+    }
+
+    for(int i = 0 ; i < n ; i ++){
+        for(int j = i ; j < n ; j++){
+            if (hoanvi[i] > hoanvi[j]){
+                int temp = hoanvi[i];
+                hoanvi[i] = hoanvi[j];
+                hoanvi[j] = temp;
+            }
+        }
+    }
+
+    for(int i = 0 ; i<n ; i ++){
+        printf("%d ",hoanvi[i]);
+    }
 }
